11988460ec4c4e64ee31e8caed8a7d9c2ff0b316
Jose Alfredo Escobar Rodriguez <mxjosealfredo@gmail.com>
2025-05-08 10:43:17 -0600

    Refactor SuperheroResponse and SuperheroSearchScreen to handle detailed superhero data and improve UI representation

--- lib/data/model/superhero_response.dart
+++ lib/data/model/superhero_response.dart
@@ -1,9 +1,18 @@
+import 'package:superhero_app/data/model/superhero_detail_response.dart';
+
 class SuperheroResponse {
   final String response;
+  final List<SuperheroDetailResponse>? results;
 
-  SuperheroResponse({required this.response});
+  SuperheroResponse({required this.response, required this.results});
 
   factory SuperheroResponse.fromJson(Map<String, dynamic> json) {
-    return SuperheroResponse(response: json['response'] as String);
+    return SuperheroResponse(
+      response: json['response'] as String,
+      results:
+          (json['results'] as List<dynamic>?)
+              ?.map((hero) => SuperheroDetailResponse.fromJson(hero))
+              .toList(),
+    );
   }
 }
--- lib/screens/superhero_search_screen.dart
+++ lib/screens/superhero_search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:superhero_app/data/model/superhero_detail_response.dart';
 import 'package:superhero_app/data/model/superhero_response.dart';
 import 'package:superhero_app/data/respository.dart';
 
@@ -34,26 +35,45 @@ class _SuperheroSearchScreenState extends State<SuperheroSearchScreen> {
               },
             ),
           ),
-          FutureBuilder(
-            future: _superHeroInfo,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error fetching data'));
-              } else if (snapshot.hasData) {
-                return Center(
-                  child: Text(
-                    'Superhero Response: ${snapshot.data?.response}',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                );
-              }
-              return Center(child: Text('No data available'));
-            },
-          ),
+          bodyList(),
         ],
       ),
+    );
+  }
+
+  FutureBuilder<SuperheroResponse?> bodyList() {
+    return FutureBuilder(
+      future: _superHeroInfo,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error fetching data'));
+        } else if (snapshot.hasData) {
+          List<SuperheroDetailResponse>? data = snapshot.data?.results;
+          return Expanded(
+            child: ListView.builder(
+              itemCount: data?.length ?? 0,
+              itemBuilder: (context, index) {
+                if (data != null && data.isNotEmpty) {
+                  return ListTile(
+                    leading: Image.network(data[index].url ?? ''),
+                    title: Text(data[index].name ?? ''),
+                    subtitle: Text("Power: ${data[index].id}"),
+                  );
+                } else {
+                  return const ListTile(
+                    leading: Icon(Icons.error),
+                    title: Text('No data available'),
+                    subtitle: Text('Please try again later'),
+                  );
+                }
+              },
+            ),
+          );
+        }
+        return Center(child: Text('No data available'));
+      },
     );
   }
 }
